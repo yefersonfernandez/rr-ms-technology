cd17c43b1cb7102e85251069456e6a0964c319af
Yeferson Fernandez <yeferson1500@gmail.com>
2025-12-29 12:41:17 -0500

    chore: initial clean architecture scaffold

---
+++ applications/app-service/src/main/java/com/onclass/technology/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package com.onclass.technology.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "com.onclass.technology.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
