9ea75b338bd7c4a7f6f51f9951657f900c7c25aa
Yeferson Fernandez <yeferson1500@gmail.com>
2026-01-05 17:43:04 -0500

    feat(domain): add capability-related exceptions and constants

--- domain/model/src/main/java/com/onclass/technology/enums/ExceptionMessages.java
+++ domain/model/src/main/java/com/onclass/technology/enums/ExceptionMessages.java
@@ -5,7 +5,10 @@ import lombok.Getter;
 @Getter
 public enum ExceptionMessages {
 
-    TECHNOLOGY_ALREADY_EXISTS("Technology with name '%s' already exists");
+    TECHNOLOGY_ALREADY_EXISTS("Technology with name '%s' already exists"),
+    INVALID_COUNT("The number of technologies must be between %d and %d"),
+    REPEATED_TECHS("There are repeated technologies in the list"),
+    TECH_NOT_FOUND("Some technology IDs do not exist");
 
     private final String message;
 
