7d2acacf7e759faa52caf88d05279d66e45c4485
Yeferson Fernandez <yeferson1500@gmail.com>
2026-01-08 19:05:19 -0500

    refactor(technology): rename CapabilityConstants to TechnologyConstants

--- domain/model/src/main/java/com/onclass/technology/constants/TechnologyConstants.java
+++ domain/model/src/main/java/com/onclass/technology/constants/TechnologyConstants.java
@@ -6,5 +6,6 @@ import lombok.experimental.UtilityClass;
 public final class CapabilityConstants {
     public static final int MIN_TECHS = 3;
     public static final int MAX_TECHS = 20;
+    public static final int ZERO_OTHER_ASSOCIATIONS = 0;
 }
 
